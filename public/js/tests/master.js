2b6b84f0236dd3d4eca4b10cdb2a4809404255a8
Scott <scott@Scotts-MacBook-Pro.local>
2013-10-18 11:43:35 -0500

    first tests

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -1,14 +1,50 @@
-// 'use strict';
+'use strict';
 
-// module('Integration Testing', {setup: setupTest, teardown: teardownTest});
+module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
-// function setupTest(){
-//   initialize(null, true);
-// }
+function setupTest(){
+  initialize(null, true);
+}
 
-// function teardownTest(){
-// }
+function teardownTest(){
+}
 
-// test('<name-of-feature>', function(){
-//   expect(1);
-// });
+test('Select Section Dropdown Exists and is Visible', function(){
+  expect(1);
+  deepEqual($('#secion-input').is(":visible"), true, '#section-input should be visible');
+});
+
+test('Seat Input Exists and is Visible', function(){
+  expect(1);
+  deepEqual($('#seats-input').is(":visible"), true, '#seats-input should be visible');
+});
+
+test('Seat Price Input Exists and is Visible', function(){
+  expect(1);
+  deepEqual($('#price-input').is(":visible"), true, '#price-input should be visible');
+});
+
+test('Create Seats Button Exists and is Visible', function(){
+  expect(1);
+  deepEqual($('#create-seats').is(":visible"), true, '#create-seats should be visible');
+});
+
+test('Create Seats Button adds seats to the proper section', function(){
+  expect(1);
+
+});
+
+test('Create Seats Button adds data-price element to the proper section', function(){
+  expect(1);
+
+});
+
+test('Create Seats Button removes prior selections', function(){
+  expect(1);
+
+});
+
+test('If no sections remain to be selected, create seats section is not visible', function(){
+  expect(1);
+
+});
