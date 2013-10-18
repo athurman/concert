a88f40ee2b06690c4fcddf53e91b59c2531f92be
Scott <scott@Scotts-MacBook-Pro.local>
2013-10-18 12:36:12 -0500

    finished create button tests

--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -13,11 +13,12 @@ function createSeats(section,seats,price){
 	$('#secion-input').val(section);
   $('#seats-input').val(seats);
   $('#price-input').val(price);
+  $('#create-seats').trigger('click');
 }
 
 function existAndVisible(element){
 	deepEqual(element.length?, true, element.selector + 'should exist');
-  deepEqual(element.is(":visible"), true, element.selector + 'should be visible');
+  deepEqual(element.is(':visible'), true, element.selector + 'should be visible');
 }
 
 test('Select Section Dropdown Exists and is Visible', function(){
@@ -44,21 +45,35 @@ test('Create Seats Button adds seats to the proper section', function(){
   expect(2);
   createSeats('vip','50',500);
   createSeats('ga','500',50);
-  deepEqual($('#vip > div').length, 50, '50 VIP seats should exist');
-  deepEqual($('#ga > div').length, 500, '500 General Addmission seats should exist');
+  deepEqual($('#vip > div').length, 50,
+  	'50 VIP seats should exist');
+  deepEqual($('#ga > div').length, 500,
+  	'500 General Addmission seats should exist');
 });
 
 test('Create Seats Button adds data-price element to the proper section', function(){
-  expect(1);
-  //createSeats(section,seats,price);
+  expect(2);
+  createSeats('vip','50',500);
+  createSeats('ga','500',50);
+  deepEqual($('#vip').data('price'), 500,
+  	'VIP section should hava a data-price element of 500');
+  deepEqual($('#ga').data('price'), 50,
+  	'General Addmission section should have a data-price element of 50');
 });
 
 test('Create Seats Button removes prior selections', function(){
   expect(1);
-  //createSeats(section,seats,price);
+  createSeats('vip','50',500);
+  deepEqual($('#section-input').val('vip'), false,
+  	'VIP selection should be removed once seats have been added');
+  createSeats('vip','50',500);
+  deepEqual($('#section-input').val('ga'), false,
+  	'General Admission selection should be removed once seats have been added');
 });
 
-test('If no sections remain to be selected, create seats section is not visible', function(){
+test('If both section have been created, then create seats section is not visible', function(){
   expect(1);
-
+  createSeats('vip','50',500);
+  createSeats('ga','500',50);
+  deepEqual($('#add-seats').is(':visible'), false, ' #add-seats should not be visible');
 });
